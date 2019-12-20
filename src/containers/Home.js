709d1c6e7768a0eca6922d386f2ecb75e46784f9
xistory <ximinseo@hotmail.com>
2019-12-20 16:48:29 +0900

    Updating the lander

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,6 +3,7 @@ import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import "./Home.css";
 import { API } from "aws-amplify";
 import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function Home(props) {
     const [notes, setNotes] = useState([]);
@@ -56,8 +57,16 @@ export default function Home(props) {
     function renderLander() {
         return (
             <div className="lander">
-              <h1>Scratch</h1>
-              <p>A simple note taking app</p>
+                <h1>Scratch</h1>
+                <p>A simple note taking app</p>
+                <div>
+                    <Link to="/login" className="btn btn-info btn-lg">
+                        Login
+                    </Link>
+                    <Link to="/signup" className="btn btn-success btn-lg">
+                        Signup
+                    </Link>
+                </div>
             </div>
         );
     }
